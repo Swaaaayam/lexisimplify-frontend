073819b28dd135264d406253ef3a2406ba7f0f1d
hp@SWAYAM <swayam24sahoo@gmail.com>
2025-08-06 19:48:08 +0530

    Fixed routing issue

--- src/pages/Simplify.jsx
+++ src/pages/Simplify.jsx
@@ -41,7 +41,7 @@ const Simplify = () => {
     setAnswer("Thinking...");
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/ask", {
+      const res = await fetch(`${BASE_URL}/ask`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question, clause }),
